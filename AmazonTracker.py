f85d77f5d8b85e0b19e807775d35aef33426de7c
Tony <tonylu23@gmail.com>
2019-09-18 00:47:32 -0400

    Revert "Revert "Updates via notifications""
    This reverts commit 6143ad80ab5442188ad7d16b36149f61e9c37d99.

--- AmazonTracker.py
+++ AmazonTracker.py
@@ -2,6 +2,7 @@ import requests
 import time
 from bs4 import BeautifulSoup
 from datetime import datetime
+from plyer import notification
 
 # the product's URL
 product_URL = 'https://www.amazon.com/Apple-MacBook-15-inch-2-3GHz-8-core/dp/B07S58MHXF'
@@ -13,14 +14,18 @@ header_agent = {"User-Agent": 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWeb
 current_price = None
 hist_lowest_price = current_price
 
+# app properties
+app_name = "Amazon Price Tracker"
+icon = 'Amazon Price Tracker\icon.ico'
+
 def check_price(hist_lowest_price, current_price):
     page = requests.get(product_URL, headers=header_agent) # go to URL
     soup = BeautifulSoup(page.content, 'html.parser') # parse URL content
 
     title = soup.find(id='productTitle').get_text().strip() # find product title
-    title = title[:25] + "..." # truncate product title
+    title = title[:25] + "..."
     price = soup.find(id='priceblock_ourprice').get_text() # find product price
-    price = price.replace(',', '') # format for float conversion
+    price = price.replace(',', '')
     current_price = float(price[1:]) # convert product price to int, record current known price
 
     print(f"[{get_time()}] Monitoring price of: {title}")
@@ -30,6 +35,14 @@ def check_price(hist_lowest_price, current_price):
     if hist_lowest_price is None:
         hist_lowest_price = current_price
     elif current_price < hist_lowest_price: # check if price dropped compared to hist_low
+        
+        # Content of notification
+        notif_desc = f"The price dropped ${(hist_lowest_price - current_price)}!\nOld Price: ${hist_lowest_price}\nNew Price: ${current_price}"
+
+        # notify the user with a notification!
+        notification.notify(title=f"Price Update for {title}!", app_name=app_name, app_icon=icon, message=notif_desc)
+
+        # log to console
         print(f"Price has dropped for {title}!")
         print(f"Old Price: ${hist_lowest_price}")
         print(f"New price: ${current_price}")
@@ -39,7 +52,7 @@ def check_price(hist_lowest_price, current_price):
 def get_time():
     return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
-# run forever :)
+# run forver :)
 while(True):
     check_price(hist_lowest_price, current_price)
     time.sleep(5 * 60)
